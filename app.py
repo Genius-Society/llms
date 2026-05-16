06db5495916872637967cda829736334327c8e61
admin <admin@noreply.com>
2026-05-16 15:01:42 +0800

    refine title

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from modules.deepseek import DeepSeek_R1_Qwen_7B
 from utils import EN_US
 
 ZH2EN = {
-    "# 大模型部署实例合集": "# LLM Deployment Instances",
+    "大模型部署实例合集": "LLM Deployment Instances",
     "API 部署聚合": "API Aggregation",
     "真实 DeepSeek R1 Qwen 7B 模型": "Real DeepSeek R1 Qwen 7B",
 }
@@ -17,5 +17,5 @@ def _L(zh_txt: str):
 gr.TabbedInterface(
     interface_list=[LLM_APIs(), DeepSeek_R1_Qwen_7B()],
     tab_names=[_L("API 部署聚合"), _L("真实 DeepSeek R1 Qwen 7B 模型")],
-    title=_L("# 大模型部署实例合集"),
+    title=_L("大模型部署实例合集"),
 ).launch(css="#gradio-share-link-button-0 { display: none; }", ssr_mode=False)
